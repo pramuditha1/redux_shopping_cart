e46a845a5a0d27120e03d03999f8324e23c4a061
pramuditha1 <pramuditha1@live.com>
2021-10-23 00:45:11 +0530

    load product details page

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,19 +1,48 @@
 import React, {useEffect} from "react";
 import { useParams } from "react-router";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProduct, removeSelectedProduct } from "../redux/actions/productActions";
 
 const ProductDetails = () => {
+    const product = useSelector((state) => state.product);
+    const {image, title, category, description} = product;
+
     const {productId} = useParams();
-    console.log(productId);
+    const dispatch = useDispatch();
+
     const fetchProductDetail = async () => {
         const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
         .catch((err) => {
             console.log(err);
-        })
-    }
+        });
+        dispatch(selectedProduct(response.data));
+    };
+
+    useEffect(() => {
+        if (productId && productId !== "") fetchProductDetail();
+
+        //clean up selected product
+        return () => {
+            dispatch(removeSelectedProduct());
+        }
+    }, [productId]);
+
+
     return (
-    <div>
-        <h1>ProductDetails</h1>
+    <div className="ui grid container">
+        {Object.keys(product).length === 0 ? (
+            <div>...Loading</div>
+        ) : (
+            <div className="ui placeholder segment">
+                <div><img src={image} /></div>
+                <div>{image}</div> 
+                <div>{title}</div> 
+                <div>{category}</div> 
+                <div>{description}</div>
+            </div>
+        )}
+        
     </div>)
 }
 
